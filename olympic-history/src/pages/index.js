97ad44efc9c4395b243df7932c9db5f46530311a
Mateus <mateuslg2000@gmail.com>
2022-11-28 19:42:30 -0300

    Terminando de integrar visualizacoes

--- olympic-history/src/pages/index.js
+++ olympic-history/src/pages/index.js
@@ -1,8 +1,8 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 
-import {TimeLine, TimeLineTitle, TimeLineBottom} from "./timeLine.js";
+import { TimeLine, TimeLineTitle, TimeLineBottom } from "./timeLine.js";
 import StaticView from "./staticView.js";
-import "./index.css"
+import "./index.css";
 import { YearContext } from "./year_context.js";
 import MapBar from "../vis/map-bar/mapBar.js";
 import BubblePlot from "../vis/bubble/bubblePlot.js";
@@ -10,29 +10,68 @@ import BubblePlot from "../vis/bubble/bubblePlot.js";
 //const YearContext = React.createContext();
 
 const TimeLineVis = () => {
+  const [year, setYear] = useState(1896);
 
-    const [year, setYear] = useState(1896)
-
-    return (
-        <YearContext.Provider value={{year, setYear}}>
-            <div className="container">
-                <TimeLineTitle/>
-                <div className="container2">
-                    <TimeLine />
-                    <StaticView />
-                    
-                </div>
-                <div className="chart">
-                    <MapBar className="mapbar"/>
-                </div>
-                <div className="chart">
-                    <BubblePlot className="mapbar"/>
-                </div>
-                <TimeLineBottom/>
-            </div>
-        </YearContext.Provider>
-    );
-    
-}
+  return (
+    <YearContext.Provider value={{ year, setYear }}>
+      <div className="container">
+        <TimeLineTitle />
+        <div className="container2">
+          <TimeLine />
+          <StaticView />
+        </div>
+        <div style={{ background: "#012030", margin: 10 }}>
+          <br />
+          <h1 style={{ color: "white" }}>
+            Mapa e Gráfico de Barras Interativo
+          </h1>
+          <br />
+          <br />
+          <p style={{ color: "white", fontSize: 20 }}>
+            No painel abaixo é mostrado um mapa que interage com o gráfico de
+            barras ao lado quando um país é selecionado. Para cada país é
+            mostrado a sua taxa de aproveitamento por esporte nas olimpíadas.
+            <br />
+            Para o cálculo da taxa de apoveitamento foram definidos pesos 1, 2 e
+            3 para as medalhas de bronze, prata e ouro. Após isso, é feito o
+            somatório das medatlas multiplicando-as pelos seus pesos
+            correspondentes.
+            <br />
+            Por fim, vamos ter essa soma dividida pelo numero de atletas
+            enviados pelo país, gerando assim a taxa de aproveitamento.
+          </p>
+          <br />
+          <br />
+          <br />
+          <br />
+        </div>
+        <div className="chart">
+          <MapBar className="mapbar" />
+        </div>
+        <div style={{ background: "#012030", margin: 10 }}>
+          <br />
+          <h1 style={{ color: "white" }}>Gráfico de Bolhas Interativo</h1>
+          <br />
+          <br />
+          <p style={{ color: "white", fontSize: 20 }}>
+            Aqui temos a visualização de um gráfico de bolhas em que seus eixos
+            x e y correspondem, respectivamente, ao total de medalhas e o total
+            de atletas enviados. Cada círculo no gráfico representa um país e
+            sua área representa o seu pib per cápita.
+            <br />
+            No gráfico, a cada década, iniciando em 1890 até 2010, são
+            representados os 15 países que possuem o maior número de medalhas. O
+            intuito da visualização é ver a relação entre numero de atletas
+            enviados, numero de medalhas ganhas e fatores economicos.
+          </p>
+        </div>
+        <div className="chart">
+          <BubblePlot className="mapbar" />
+        </div>
+        <TimeLineBottom />
+      </div>
+    </YearContext.Provider>
+  );
+};
 
 export default TimeLineVis;
--- olympic-history/src/vis/women/pieChart.js
+++ olympic-history/src/vis/women/pieChart.js
@@ -1,63 +1,87 @@
-import React, {useRef, useEffect, useState} from "react";
-import {Runtime, Inspector} from "@observablehq/runtime";
+import React, { useRef, useEffect, useState } from "react";
+import { Runtime, Inspector } from "@observablehq/runtime";
 import notebook from "f7f4ac94baa60885";
-import "./index.css"
 
 import { YearContext } from "./../../pages/year_context.js";
+import "./index.css";
 
-function PieChart () {
+function PieChart() {
   const yearCurRef = useRef();
   const piechartRef = useRef();
-  const outersvgRef = useRef();
-  const svgRef = useRef();
-  const piechartmalefemaleRef = useRef();
   const barsRef = useRef();
-  const svgBarRef = useRef();
-  const cssRef = useRef();
+  const bRef = useRef();
 
   const [runtime, setRuntime] = useState();
   const [main, setMain] = useState();
 
-  const {year} = React.useContext(YearContext);
+  const { year } = React.useContext(YearContext);
 
   useEffect(() => {
     setRuntime(new Runtime());
   }, []);
 
   useEffect(() => {
-    if(runtime){
-        const m = runtime.module(notebook, name => {
-            if (name === "yearCur") return new Inspector(yearCurRef.current);
-            if (name === "piechart") return new Inspector(piechartRef.current);
-            if (name === "outersvg") return new Inspector(outersvgRef.current);
-            if (name === "svg") return new Inspector(svgRef.current);
-            if (name === "piechartmalefemale") return new Inspector(piechartmalefemaleRef.current);
-            if (name === "bars") return new Inspector(barsRef.current);
-            if (name === "svgBar") return new Inspector(svgBarRef.current);
-            if (name === "css") return new Inspector(cssRef.current);
-            return ["show","show2","barras1"].includes(name);
-          });
-          setMain(m);
-          return () => runtime.dispose();
-    } 
+    if (runtime) {
+      const m = runtime.module(notebook, (name) => {
+        if (name === "yearCur") return new Inspector(yearCurRef.current);
+        if (name === "piechart") return new Inspector(piechartRef.current);
+        if (name === "bars") return new Inspector(barsRef.current);
+        if (name === "b") return new Inspector(bRef.current);
+        return ["show", "show2", "tree"].includes(name);
+      });
+      setMain(m);
+      return () => runtime.dispose();
+    }
   }, [runtime]);
 
-  if(main){
+  if (main) {
     main.redefine("yearCur", year);
   }
-
   return (
-    <>
-      <div style={{display:"None"}} ref={yearCurRef} />
-      <div className="pie" ref={piechartRef} />
-      <div style={{display:"None"}} ref={outersvgRef} />
-      <div style={{display:"None"}} ref={svgRef} />
-      <div style={{display:"None"}} ref={piechartmalefemaleRef} />
-      <div className="bar" ref={barsRef} />
-      <div ref={svgBarRef} />
-      <div ref={cssRef} />
-    </>
+    <div style={{ width: "100%", height: "100%" }}>
+      <div style={{ display: "none" }} ref={yearCurRef} />
+      <div style={{ display: "flex" }}>
+        <div className="pie" ref={piechartRef} />
+        <div className="bar" ref={barsRef} />
+      </div>
+      <div className="tree" ref={bRef} />
+    </div>
   );
 }
 
 export default PieChart;
+
+// import React, {useRef, useEffect} from "react";
+// import {Runtime, Inspector} from "@observablehq/runtime";
+// import notebook from "f7f4ac94baa60885";
+
+// function Notebook() {
+//   const yearCurRef = useRef();
+//   const piechartRef = useRef();
+//   const barsRef = useRef();
+//   const bRef = useRef();
+
+//   useEffect(() => {
+//     const runtime = new Runtime();
+//     runtime.module(notebook, name => {
+//       if (name === "yearCur") return new Inspector(yearCurRef.current);
+//       if (name === "piechart") return new Inspector(piechartRef.current);
+//       if (name === "bars") return new Inspector(barsRef.current);
+//       if (name === "b") return new Inspector(bRef.current);
+//       return ["show","show2","tree"].includes(name);
+//     });
+//     return () => runtime.dispose();
+//   }, []);
+
+//   return (
+//     <>
+//       <div ref={yearCurRef} />
+//       <div ref={piechartRef} />
+//       <div ref={barsRef} />
+//       <div ref={bRef} />
+//       <p>Credit: <a href="https://observablehq.com/d/f7f4ac94baa60885">Time Line - mulheres by Mateus Gonçalves</a></p>
+//     </>
+//   );
+// }
+
+// export default Notebook;
