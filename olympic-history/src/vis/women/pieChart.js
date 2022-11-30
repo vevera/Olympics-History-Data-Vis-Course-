b2405a1b5af7771854f5f45f00bff259f5e30759
Mateus <mateuslg2000@gmail.com>
2022-11-29 21:44:31 -0300

    Terminando alguns outras coisas

--- olympic-history/src/vis/women/pieChart.js
+++ olympic-history/src/vis/women/pieChart.js
@@ -38,50 +38,25 @@ function PieChart() {
     main.redefine("yearCur", year);
   }
   return (
-    <div style={{ width: "100%", height: "100%" }}>
+    <div style={{ width: "100%", height: "100%", display:"flex" , flex: 7, flexDirection:"column"}}>
       <div style={{ display: "none" }} ref={yearCurRef} />
-      <div style={{ display: "flex" }}>
-        <div className="pie" ref={piechartRef} />
-        <div className="bar" ref={barsRef} />
+
+      <div style={{flex: 3, position:"relative"}} className="tree" ref={bRef} />
+     
+     
+      <div style={{ display: "flex", flex:4, flexDirection:"row" }}>
+        {/* <div style={{/*transform: "translate(0%, 6%)",*/ /*display:"flex", flexDirection:"column", alignItems:"center"}}> */}
+          {/* <h3 style={{/*transform: "translate(20%, -450%)"}>Proporção Homens x Mulheres </h3>*/ }
+          <div style={{position:"relative", flex:2}} className="pie" ref={piechartRef} />
+        {/* </div> */}
+        {/* <div style={{/*display:"flex", flexDirection:"column", alignItems:"center", justifyContent:"center", flex:2} }> */}
+          <div style={{position:"relative", flex:2}} className="bar" ref={barsRef} />
+          {/* <h3 style={{transform: "translate(-45%, -600%)",marginBottom:"5%"}}>Distribuição de Modalidades </h3> */}
+        {/* </div> */}
       </div>
-      <div className="tree" ref={bRef} />
+      
     </div>
   );
 }
 
 export default PieChart;
-
-// import React, {useRef, useEffect} from "react";
-// import {Runtime, Inspector} from "@observablehq/runtime";
-// import notebook from "f7f4ac94baa60885";
-
-// function Notebook() {
-//   const yearCurRef = useRef();
-//   const piechartRef = useRef();
-//   const barsRef = useRef();
-//   const bRef = useRef();
-
-//   useEffect(() => {
-//     const runtime = new Runtime();
-//     runtime.module(notebook, name => {
-//       if (name === "yearCur") return new Inspector(yearCurRef.current);
-//       if (name === "piechart") return new Inspector(piechartRef.current);
-//       if (name === "bars") return new Inspector(barsRef.current);
-//       if (name === "b") return new Inspector(bRef.current);
-//       return ["show","show2","tree"].includes(name);
-//     });
-//     return () => runtime.dispose();
-//   }, []);
-
-//   return (
-//     <>
-//       <div ref={yearCurRef} />
-//       <div ref={piechartRef} />
-//       <div ref={barsRef} />
-//       <div ref={bRef} />
-//       <p>Credit: <a href="https://observablehq.com/d/f7f4ac94baa60885">Time Line - mulheres by Mateus Gonçalves</a></p>
-//     </>
-//   );
-// }
-
-// export default Notebook;
